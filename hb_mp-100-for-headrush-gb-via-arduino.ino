5a2bd4d9205bcabf11fdb89ecbbd1e95a8210092
Dmitry Ledentsov <dmlled@yahoo.com>
2020-10-20 11:40:07 +0200

    debugging made optional

--- hb_mp-100-for-headrush-gb-via-arduino.ino
+++ hb_mp-100-for-headrush-gb-via-arduino.ino
@@ -4,6 +4,9 @@
 // Create and bind the MIDI interface to the default hardware Serial port
 MIDI_CREATE_DEFAULT_INSTANCE();
 
+// uncomment this for debugging via the LED
+//#define DEBUG 1
+
 // the setup function runs once when you press reset or power the board
 void setup() {
   Serial.begin(19200);
@@ -18,14 +21,18 @@ void setup() {
 
 void ccFunc(byte channel, byte number, byte value) {
   // re-map the value from 127 to 0 for HB MP100 -> Headrush GB
-  if (value==127) {
+  if (value == 127) {
     value = 0;
   }
   MIDI.sendControlChange(number, value, channel);
+
+#ifdef DEBUG
+  blinkNumberInReverseOrder(channel);
+  blinkNumberInReverseOrder(number);
+  blinkNumberInReverseOrder(value);
+#else
   blinkShortly();
-  //  blinkNumberInReverseOrder(channel);
-  //  blinkNumberInReverseOrder(number);
-  //  blinkNumberInReverseOrder(value);
+#endif
 }
 
 void blinkShortly() {
@@ -36,14 +43,23 @@ void blinkShortly() {
 
 // use when debugging
 void blinkNumberInReverseOrder(byte number) {
+  int delayMs = 300;
   while (number > 0) {
     byte lastDigit = number % 10;
     number = number / 10;
+
+    if (lastDigit == 0) {
+      delayMs = 100;
+      lastDigit = 10;
+    } else {
+      delayMs = 300;
+    }
+
     for (byte i = 0; i < lastDigit; i++) {
       digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-      delay(300);                       // wait for a second
+      delay(delayMs);                       // wait for a second
       digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-      delay(300);
+      delay(delayMs);
     }
     delay(1000);
   }
